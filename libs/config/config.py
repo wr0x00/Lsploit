7f69c722affa823979e027ecc6e3228a801b8724
wr <86941613+wr0x00@users.noreply.github.com>
2024-05-02 04:03:18 +0000

    Changes to be committed: 	modified:   libs/config/config.py 	new file:   libs/config/others.yml

--- libs/config/config.py
+++ libs/config/config.py
@@ -1,10 +1,43 @@
+import yaml
 
 class Config(object):
     def __init__(self) -> None:
         super().__init__()
 
-        import yaml
-        with open("libs/config/keys.yml", encoding='utf-8') as fkeys:
-            self.fqfa_key = yaml.load(fkeys.read(), Loader=yaml.FullLoader)["keys"]["fqfa_key"]
-            self.shodan_key = yaml.load(fkeys.read(), Loader=yaml.FullLoader)["keys"]["shodan_key"]
-            
+        self.fkeys   = "libs/config/keys.yml"
+        self.fothers = "libs/config/others.yml"
+
+
+        self.fqfa_key   = self.get_config_file(open(self.fkeys))["keys"]["fqfa_key"]
+        self.shodan_key = self.get_config_file(open(self.fkeys))["keys"]["shodan_key"]
+        
+        self.first      =  self.get_config_file(open(self.fothers))["others"]["first"]
+        self.news       =  self.get_config_file(open(self.fothers))["others"]["news"]  
+        self.language   =  self.get_config_file(open(self.fothers))["others"]["language"]
+        self.proxy      =  self.get_config_file(open(self.fothers))["others"]["proxy"]    
+    
+    @staticmethod
+    def get_config_file(content: dict) -> dict:
+        return yaml.safe_load(content)
+    
+    @staticmethod
+    def change_config_file(content,file,key,data)->None:
+        s = Config().get_config_file(open(content))
+        s[file][key] = data
+        yaml.safe_dump(s,open(content,'w'))
+'''
+a=Config()
+#print(a.get_config_file(open("libs/config/others.yml"))["others"]["first"])
+print(a.fqfa_key)
+print(a.shodan_key)
+
+print(a.language)
+print(a.proxy)
+print(a.first)
+print(a.news)
+print(type(a.language))
+print(type(a.proxy))
+print(type(a.first))
+print(type(a.news))
+Config().change_config_file(content=a.fothers,file="others",key="news",data=10)
+'''
