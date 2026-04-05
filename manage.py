e19a680407961a14a8179fc1ada1a08dbf969cb2
wr <86941613+wr0x00@users.noreply.github.com>
2026-04-05 17:25:58 +0800

    Clean up blank lines in manage.py
    Removed extra blank lines for cleaner code.

--- manage.py
+++ manage.py
@@ -2,8 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lsploit.settings')
@@ -16,7 +14,5 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
 if __name__ == '__main__':
     main()
