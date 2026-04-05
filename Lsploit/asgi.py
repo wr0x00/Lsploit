7e399c829b54ef13f33fcb9b7ff940e3b4eb022e
wr <86941613+wr0x00@users.noreply.github.com>
2026-04-05 17:26:59 +0800

    Update asgi.py

--- Lsploit/asgi.py
+++ Lsploit/asgi.py
@@ -6,11 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lsploit.settings")
-
 application = get_asgi_application()
