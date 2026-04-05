f9cd5e16818abdbc4db89de798cd3a07f609d566
wr <86941613+wr0x00@users.noreply.github.com>
2026-04-05 17:27:18 +0800

    Clean up blank lines in wsgi.py
    Removed unnecessary blank lines for cleaner code.

--- Lsploit/wsgi.py
+++ Lsploit/wsgi.py
@@ -6,11 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lsploit.settings")
-
 application = get_wsgi_application()
